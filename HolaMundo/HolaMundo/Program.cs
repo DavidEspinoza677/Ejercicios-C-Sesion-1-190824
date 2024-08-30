b199da381f95a2e79c3d81fff1a912fc9b36d292
David Espinoza <165340445+DavidEspinoza677@users.noreply.github.com>
2024-08-30 12:21:51 -0600

    Ejerciciso C#primera sesion 190824

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HolaMundo
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola desde la UAM");
+            Console.WriteLine("Mi nombre es David Espinoza");
+            Console.WriteLine("Tengo 19, soy estudiante de primer año de sistemas.");
+            Console.WriteLine("Soy parte de la comunidad de Artemis Devs en la universidad americana UAM");
+        }
+    }
+}
